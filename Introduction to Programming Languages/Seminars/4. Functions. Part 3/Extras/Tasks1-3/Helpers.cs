8913371bb61a0d550fe1b6629416ab6356a39fa4
nadia3373 <3773373@mail.ru>
2022-09-29 13:26:24 +0700

    Changed array display

--- Introduction to Programming Languages/Seminars/4. Functions. Part 3/Extras/Tasks1-3/Helpers.cs
+++ Introduction to Programming Languages/Seminars/4. Functions. Part 3/Extras/Tasks1-3/Helpers.cs
@@ -23,6 +23,13 @@ namespace Extras
             return counts;
         }
 
+        public static int[] FillArray(int[] arr)
+        {
+            Random random = new Random();
+            for (int count = 0; count < arr.Length; count++) arr[count] = random.Next(1, 100);
+            return arr;
+        }
+
         public static void Hashes()
         {
             Console.WriteLine();
@@ -30,50 +37,47 @@ namespace Extras
             Console.WriteLine();
         }
 
-        public static int[] PopulateArray(int[] arr, int[] quantity = null, string type = null)
+        public static char[] PopulateArray(char[] arr, int[] quantity, string type)
         {
+            for (int count = 0; count < arr.Length; count++) arr[count] = '.';
             Random random = new Random();
-            if (type == "random" || type == "consecutive")
+            int limit = quantity[0] + quantity[1];
+            int[] indices = new int[arr.Length];
+            for (int count = 0; count < limit; count++)
             {
-                int limit = quantity[0] + quantity[1];
-                int[] indices = new int[arr.Length];
-                for (int count = 0; count < limit; count++)
+                // Если количество нулей и единиц не исчерпано и тип заполнения - случайный, генерируется 0 или 1.
+                // Если исчерпано количество нулей или единиц, выбирается тот элемент, который не исчерпан.
+                char choice = '1';
+                if (quantity[0] > 0 && quantity[1] > 0 && type == "random") choice = Convert.ToChar(random.Next(48, 50));
+                else if (quantity[0] > 0) choice = '0';
+                // Число 0 или 1 помещается в ячейку массива по сгенерированному адресу только в том случае, если найдена ячейка, которая ещё не занята.
+                // Иначе генерируется другой адрес ячейки.
+                if (type == "random")
                 {
-                    // Если количество нулей и единиц не исчерпано и тип заполнения - случайный, генерируется 0 или 1.
-                    // Если исчерпано количество нулей или единиц, выбирается тот элемент, который не исчерпан.
-                    int choice = 1;
-                    if (quantity[0] > 0 && quantity[1] > 0 && type == "random") choice = random.Next(2);
-                    else if (quantity[0] > 0) choice = 0;
-                    // Число 0 или 1 помещается в ячейку массива по сгенерированному адресу только в том случае, если найдена ячейка, которая ещё не занята.
-                    // Иначе генерируется другой адрес ячейки.
-                    if (type == "random")
+                    int index;
+                    while (true)
                     {
-                        int index;
-                        while (true)
+                        index = random.Next(arr.Length);
+                        if (indices[index] == 0)
                         {
-                            index = random.Next(arr.Length);
-                            if (indices[index] == 0)
-                            {
-                                arr[index] = choice;
-                                indices[index] = 1;
-                                break;
-                            }
+                            arr[index] = choice;
+                            indices[index] = 1;
+                            break;
                         }
                     }
-                    else arr[count] = choice;
-                    quantity[choice]--;
                 }
+                else arr[count] = choice;
+                quantity[(int)choice - 48]--;
             }
-            else for (int count = 0; count < arr.Length; count++) arr[count] = random.Next(1, 100);
             return arr;
         }
 
-        public static void PrintArray(int[] arr)
+        public static void PrintArray<T>(T[] arr)
         {
             for (int count = 0; count < arr.Length; count++)
             {
-                if (count == arr.Length - 1) Console.WriteLine(arr[count]);
-                else Console.Write($"{arr[count]}, ");
+                if (count == arr.Length - 1) Console.WriteLine($"{arr[count]}");
+                else Console.Write($"{arr[count]} ");
             }
         }
     }
