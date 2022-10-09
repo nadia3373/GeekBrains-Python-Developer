24e30cead0b088c51f46084709064f672897b2dd
nadia3373 <3773373@mail.ru>
2022-10-09 23:52:07 +0700

    Added Extras 7

---
+++ Introduction to Programming Languages/Seminars/7. How not to Write Code. Part 1/Extras/Helpers.cs
@@ -0,0 +1,155 @@
+using System;
+
+namespace Extras
+{
+    class Helpers
+    {
+        #region Common
+        public static void FillArray(int[,] arr, int min, int max)
+        {
+            Random random = new Random();
+            int firstLength = arr.GetLength(0), secondLength = arr.GetLength(1);
+            for (int firstCount = 0; firstCount < firstLength; firstCount++)
+            for (int secondCount = 0; secondCount < secondLength; secondCount++) arr[firstCount, secondCount] = random.Next(min, max);
+        }
+
+        public static void Hashes()
+        {
+            Console.WriteLine();
+            Console.WriteLine("####################################################################################################");
+            Console.WriteLine();
+        }
+
+        public static void PrintArray<T>(T[,] arr, int posX = -1)
+        {
+            if (posX != -1) Console.SetCursorPosition(posX, Console.CursorTop);
+            Console.WriteLine();
+            Random random = new Random();
+            int firstLength = arr.GetLength(0), secondLength = arr.GetLength(1);
+            for (int firstCount = 0; firstCount < firstLength; firstCount++)
+            {
+                if (posX != -1) Console.SetCursorPosition(posX, Console.CursorTop);
+                for (int secondCount = 0; secondCount < secondLength; secondCount++) Console.Write($"{arr[firstCount, secondCount]}\t");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        public static void PrintArray(int[] arr)
+        {
+            for (int count = 0; count < arr.Length; count++)
+            {
+                if (arr.Length == 1) Console.WriteLine($"{arr[count]}");
+                else if (count == arr.Length - 1) Console.WriteLine($"{arr[count]}]");
+                else if (count == 0) Console.Write($"[{arr[count]}, ");
+                else Console.Write($"{arr[count]}, ");
+            }
+        }
+        #endregion
+
+        #region Task1
+        public static int[,] MultiplyMatrices(int[,] matrix1, int[,] matrix2)
+        {
+            int rows1 = matrix1.GetLength(0), cols1 = matrix1.GetLength(1), rows2 = matrix2.GetLength(0), cols2 = matrix2.GetLength(1);
+            int[,] result = new int[rows1, cols2];
+            for(int firstCount = 0; firstCount < rows1; firstCount++)
+            {
+                for(int secondCount = 0; secondCount < cols2; secondCount++)
+                {
+                    int sum = 0;
+                    for (int thirdCount = 0; thirdCount < cols1; thirdCount++) sum += matrix1[firstCount, thirdCount] * matrix2[thirdCount, secondCount];
+                    result[firstCount, secondCount] = sum;
+                }
+            }
+            return result;
+        }
+        #endregion
+
+        #region Task2
+        public static int[] FindNumbers(int[,] arr)
+        {
+            int[] result = Array.Empty<int>();
+            for (int count = 0; count < arr.GetLength(0); count++)
+            {
+                for (int index = 0; index < arr.GetLength(1); index++)
+                {
+                    if (DigitsProduct(arr[count, index]) < DigitsSum(arr[count, index]))
+                    {
+                        Array.Resize(ref result, result.Length + 1);
+                        result[result.Length - 1] = arr[count, index];
+                    }
+                }
+            }
+            return result;
+        }
+
+        public static int DigitsProduct(int number)
+        {
+            int digits = (int)Math.Floor(Math.Log10(number));
+            int result = 1;
+            for (int count = digits; count >= 0; count--) result *= (int)(number / Math.Pow(10, count) % 10);
+            return result;
+        }
+
+        public static int DigitsSum(int number)
+        {
+            int digits = (int)Math.Floor(Math.Log10(number));
+            int result = 0;
+            for (int count = digits; count >= 0; count--) result += (int)(number / Math.Pow(10, count) % 10);
+            return result;
+        }
+        #endregion
+        #region Task3
+        public static void FillArray(string[,] arr, int min, int max)
+        {
+            Random random = new Random();
+            int firstLength = arr.GetLength(0), secondLength = arr.GetLength(1);
+            for (int firstCount = 0; firstCount < firstLength; firstCount++)
+            for (int secondCount = 0; secondCount < secondLength; secondCount++) arr[firstCount, secondCount] = Convert.ToString(random.Next(min, max));
+        }
+        
+        public static bool FindPath(string[,] arr, int x, int y, bool[,] check)
+        {
+            if (x == arr.GetLength(0) - 1 && y == arr.GetLength(1) - 1) return true;
+            check[x, y] = true;
+            bool esc = false;
+            if (validate(arr, x, y - 1, check)) esc = FindPath(arr, x, y - 1, check);
+            if (!esc && validate(arr, x, y + 1, check)) esc = FindPath(arr, x, y + 1, check);
+            if (!esc && validate(arr, x - 1, y, check)) esc = FindPath(arr, x - 1, y, check);
+            if (!esc && validate(arr, x + 1, y, check)) esc = FindPath(arr, x + 1, y, check);
+            return esc;
+        }
+
+        public static void PrintWithoutGaps(string[,] arr)
+        {
+            Random random = new Random();
+            int firstLength = arr.GetLength(0), secondLength = arr.GetLength(1);
+            for (int secondCount = 0; secondCount <= secondLength + 1; secondCount++) Console.Write("_");
+            Console.WriteLine();
+            for (int firstCount = 0; firstCount < firstLength; firstCount++)
+            {
+                Console.Write('|');
+                for (int secondCount = 0; secondCount < secondLength; secondCount++)
+                {
+                    if (firstCount == 0 && secondCount == 0) Console.Write(" |");
+                    else if (firstCount == firstLength - 1 && secondCount == secondLength - 1) Console.Write(" |");
+                    else Console.Write($"{(arr[firstCount, secondCount] == "0" ? "0" : " ")}|");
+                }
+                Console.WriteLine();
+            }
+            for (int secondCount = 0; secondCount <= secondLength + 1; secondCount++) Console.Write("\u203E");
+            Console.WriteLine();
+        }
+
+        public static bool validate(string[,] arr, int x, int y, bool[,] check)
+        {
+            if (x < 0 || x >= arr.GetLength(0)) return false;
+            if (y < 0 || y >= arr.GetLength(1)) return false;
+            if (arr[x, y] == "0") return false;
+            if (check[x, y] == true) return false;
+            
+            return true;
+        }
+        #endregion
+    }
+}
---
+++ Introduction to Programming Languages/Seminars/7. How not to Write Code. Part 1/Extras/Tasks.cs
@@ -0,0 +1,70 @@
+using System;
+
+namespace Extras
+{
+    class Tasks
+    {
+        public static void Task1()
+        {
+            Console.Clear();
+            Random random = new Random();
+            int rows = random.Next(2, 9), columns = random.Next(2, 9);
+            int[,] matrix1 = new int[rows, columns], matrix2 = new int[columns, rows];
+            Console.WriteLine("Задача 1. Даны две матрицы, количество строк и столбцов которых может быть 3 или 4, заполнены числами от -9 до 9. Выполните умножение матриц.\n" +
+                              $"Дополнительный материал по задаче: https://portal.tpu.ru/SHARED/k/KONVAL/Sites/Russian_sites/1/04.htm\n\n" +
+                               $"Матрица 1 размером {rows}x{columns}: ");
+            Helpers.FillArray(matrix1, -9, 10);
+            Helpers.FillArray(matrix2, -9, 10);
+            Helpers.PrintArray(matrix1);
+            Console.SetCursorPosition(72, 3);
+            Console.WriteLine($"Матрица 2 размером {columns}x{rows}: ");
+            Helpers.PrintArray(matrix2, 72);
+            int[,] result = Helpers.MultiplyMatrices(matrix1, matrix2);
+            Console.SetCursorPosition(144, 3);
+            Console.WriteLine("Результат умножения матриц: ");
+            Helpers.PrintArray(result, 144);
+            Console.SetCursorPosition(Console.CursorLeft, 5 + Math.Max(rows, columns));
+            Helpers.Hashes();
+        }
+
+        public static void Task2()
+        {
+            Console.Clear();
+            Random random = new Random();
+            int rows = random.Next(2, 9), columns = random.Next(2, 9);
+            int[,] numbers = new int[rows, columns];
+            Helpers.FillArray(numbers, 100, 1000);
+            Console.WriteLine("Задача 2. Двумерный массив размером 3х4 заполнен числами от 100 до 9999. " +
+                              $"Найдите в этом массиве и сохраните в одномерный массив все числа, сумма цифр которых больше их произведения. Выведите оба массива.\n\n" +
+                              $"Заполненный исходный массив размером {rows}x{columns}: ");
+            Helpers.PrintArray(numbers);
+            int[] result = Helpers.FindNumbers(numbers);
+            if (result.Length > 0)
+            {
+                Console.Write("Все числа из исходного массива, сумма цифр которых больше их произведения: ");
+                Helpers.PrintArray(result);
+            }
+            else Console.WriteLine($"В исходном массиве нет чисел, сумма цифр которых больше их произведения.");
+        }
+
+        public static void Task3()
+        {
+            Console.Clear();
+            Random random = new Random();
+            int rows = random.Next(2, 9), columns = random.Next(2, 9);
+            string[,] numbers = new string[rows, columns];
+            Helpers.FillArray(numbers, 0, 2);
+            numbers[0, 0] = "1";
+            numbers[numbers.GetLength(0) - 1, numbers.GetLength(1) - 1] = "1";
+            Helpers.PrintArray(numbers);
+            Console.WriteLine("Задача 3. Двумерный массив размером 5х5 заполнен случайными нулями и единицами. Игрок может ходить только по полям, заполненным единицами. " +
+                            $"Проверьте, существует ли путь из точки [0, 0] в точку [4, 4] (эти поля требуется принудительно задать равными единице).\n" +
+                            $"Массив размеров {rows}x{columns}");
+            Helpers.PrintWithoutGaps(numbers);
+            bool[,] check = new bool[numbers.GetLength(0), numbers.GetLength(1)];
+            for (int count = 0; count < check.GetLength(0); count++)
+            for (int index = 0; index < check.GetLength(1); index++) check[count, index] = false;
+            Console.WriteLine($"{(Helpers.FindPath(numbers, 0, 0, check) ? "Из лабиринта есть выход." : "Из лабиринта нет выхода.")}");
+        }
+    }
+}
