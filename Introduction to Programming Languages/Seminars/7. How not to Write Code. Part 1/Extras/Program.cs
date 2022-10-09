e8e74e65a06fbc1587fa464df35ab99b29808e8d
nadia3373 <3773373@mail.ru>
2022-10-10 00:54:23 +0700

    Optimized output

--- Introduction to Programming Languages/Seminars/7. How not to Write Code. Part 1/Extras/Program.cs
+++ Introduction to Programming Languages/Seminars/7. How not to Write Code. Part 1/Extras/Program.cs
@@ -1,12 +1,16 @@
-﻿namespace Extras
+﻿using System;
+
+namespace Extras
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // Tasks.Task1();
-            // Tasks.Task2();
-            Tasks.Task3();
+            Tasks.Task1();
+            Console.Write("Нажмите 'Enter' для перехода к следующему заданию.");
+            if (Console.ReadKey().Key == ConsoleKey.Enter) Tasks.Task2();
+            Console.Write("Нажмите 'Enter' для перехода к следующему заданию.");
+            if (Console.ReadKey().Key == ConsoleKey.Enter) Tasks.Task3();
         }
     }
 }
