5dd2d1e8ba71dfabee1cc9efdc2d986873c85ffa
nadia3373 <3773373@mail.ru>
2022-10-23 19:56:07 +0700

    Uncommented a task

--- Introduction to Programming Languages/Seminars/9. How not to Write Code. Part 3/Extras/Program.cs
+++ Introduction to Programming Languages/Seminars/9. How not to Write Code. Part 3/Extras/Program.cs
@@ -9,8 +9,8 @@ namespace Extras
             Tasks.Task1();
             Console.Write("\nНажмите 'Enter' для перехода к следующему заданию.");
             if (Console.ReadKey().Key == ConsoleKey.Enter) Tasks.Task2();
-            // Console.Write("\nНажмите 'Enter' для перехода к следующему заданию.");
-            // if (Console.ReadKey().Key == ConsoleKey.Enter) Tasks.Task2Alt();
+            Console.Write("\nНажмите 'Enter' для перехода к следующему заданию.");
+            if (Console.ReadKey().Key == ConsoleKey.Enter) Tasks.Task2Alt();
             Console.Write("\nНажмите 'Enter' для перехода к следующему заданию.");
             if (Console.ReadKey().Key == ConsoleKey.Enter) Tasks.Task3();
             Console.Write("\nНажмите 'Enter' для перехода к следующему заданию.");
