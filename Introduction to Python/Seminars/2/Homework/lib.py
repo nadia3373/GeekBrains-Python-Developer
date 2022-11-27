9643ff72e633ab5b9759f6d412a416b0088e8516
nadia3373 <3773373@mail.ru>
2022-11-27 17:32:09 +0700

    added homework 2

---
+++ Introduction to Python/Seminars/2/Homework/lib.py
@@ -0,0 +1,53 @@
+import time
+
+
+def sum_digits(n):
+    """
+    Функция суммирования цифр
+    """
+    sum = power = 0
+    while (n * 10 ** (power + 1)) % 10 != 0: power += 1
+    n *= 10 ** power
+    while n >= 1:
+        sum += int(n % 10)
+        n //= 10
+    return sum
+
+
+def multiply(n):
+    """
+    Функция умножения чисел в диапазоне от 1 до n
+    """
+    result = n
+    for i in range(1, n):
+        result *= i
+    return result
+
+
+def rand_number(rng):
+    """
+    Функция рандомной генерации числа.
+    Принимает граничное значение, в пределах которого будет генерироваться число.
+    Для генерации случайного числа из системного времени выделяется наиболее быстро изменяющаяся часть и делится по модулю на диапазон.
+    """
+    return int(((time.time() - int(time.time())) * 1000000) % rng)
+
+
+def read_from_file(filename):
+    """
+    Функция чтения из файла
+    """
+    f = open(filename, "r")
+    indices = f.read().split()
+    f.close()
+    return indices
+
+
+def write_to_file(filename, length, quantity):
+    """
+    Функция записи в файл
+    """
+    f = open(filename, "w")
+    for _ in range(quantity):
+        f.write(f"{rand_number(length)}\n")
+    f.close()
---
+++ Introduction to Python/Seminars/2/Homework/tasks.py
@@ -0,0 +1,63 @@
+import lib
+
+def task1():
+    """
+    Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+    """
+    n = float(input("Введите число: "))
+    print(f"Сумма цифр числа {n} равна {lib.sum_digits(n)}")
+
+
+def task2():
+    """
+    Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
+    """
+    n = int(input("Введите N: "))
+    for i in range(1, n + 1):
+        print(f"Произведение чисел от 1 до {i} = {lib.multiply(i)}")
+
+
+def task3():
+    """
+    Задайте словарь из n чисел последовательности (1 + (1/n))^n и выведите на экран их сумму.
+    """
+    numbers = {n: round((1 + (1 / n)) ** n, 2) for n in range(1, int(input("Введите количество элементов последовательности: ")) + 1)}
+    print(f"Полученный словарь: {numbers}, сумма элементов = {sum(numbers.values())}")
+
+
+def task4():
+    """
+    Задайте список из N элементов, заполненных числами из промежутка [-N, N]. Найдите произведение элементов на указанных позициях. Позиции хранятся в файле file.txt в одной строке одно число.
+    """
+    num = int(input("Введите N: "))
+    step = -1 if num < -num else 1
+    numbers = [i for i in range(-num, num + step, step)]
+    print(f"Список: {numbers}")
+    lib.write_to_file("file.txt", len(numbers), 3)
+    indices = lib.read_from_file("file.txt")
+    result = numbers[int(indices[0])]
+    for i in range(1, len(indices)):
+        result *= numbers[int(indices[i])]
+    print(f"Произведение элементов списка на индексах {indices} = {result}")
+    
+
+
+def task5():
+    """
+    Реализуйте алгоритм генерации случайного числа.(Не используя библиотеки связанные с рандомом)
+    """
+    print("Случайные сгенерированные числа: ", end = "")
+    for _ in range(10):
+        print(f"{lib.rand_number(20)} ", end = "")
+    print()
+
+
+task1()
+if (input("\nНажмите Y для перехода к следующему заданию: ").lower() == "y"):
+    task2()
+if (input("\nНажмите Y для перехода к следующему заданию: ").lower() == "y"):
+    task3()
+if (input("\nНажмите Y для перехода к следующему заданию: ").lower() == "y"):
+    task4()
+if (input("\nНажмите Y для перехода к следующему заданию: ").lower() == "y"):
+    task5()
